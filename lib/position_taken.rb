add270d702c41000c03af1cd0fcb955be2573849
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-19 09:38:42 -0400

    refactored

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,6 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, position)
-  if board[position] == "X" || board[position] == "O"
-    true
-  else
-    false
-  end
+  var = board[position]
+  var == " " || var == nil || var == "" ? false : true
 end
